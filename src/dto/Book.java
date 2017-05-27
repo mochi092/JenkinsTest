388e2312c662f0284c0f7057776b200a1c831bf4
D.K <on.registry@gmail.com>
2017-05-28 08:52:19 +0900

    book

---
+++ src/dto/Book.java
@@ -0,0 +1,12 @@
+package dto;
+
+public class Book {
+	String isbn;
+
+	public void setIsbn(String isbn){
+		if(isbn == null || isbn.length() ==0 ){
+			throw new IllegalArgumentException();
+		}
+		this.isbn = isbn;
+	}
+}
