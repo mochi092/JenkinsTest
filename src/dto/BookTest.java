872990325657a9dc86ea81e4c5f0be81069835d0
D.K <on.registry@gmail.com>
2017-05-28 13:48:22 +0900

    gfgfg

--- src/dto/BookTest.java
+++ src/dto/BookTest.java
@@ -1,6 +1,5 @@
 package dto;
 
-import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -11,7 +10,8 @@ public class BookTest {
 	public void 適切なISBNを設定できる(){
 		Book book = new Book();
 		book.setIsbn("1234");
-		assertThat( book.getIsbn() , is("1234"));
+		assertEquals("1234",book.getIsbn());
+
 	}
 
 	@Test(expected=java.lang.IllegalArgumentException.class)
