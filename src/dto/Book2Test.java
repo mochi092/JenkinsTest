be876746c1ad44588610949a18c1cdc9974fe22c
D.K <on.registry@gmail.com>
2017-05-28 13:30:38 +0900

    dsdfdsf

---
+++ src/dto/Book2Test.java
@@ -0,0 +1,21 @@
+package dto;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Book2Test {
+
+	@Test
+	public void 適切なISBNを設定できる(){
+		Book book = new Book();
+		book.setIsbn("1234");
+		assertThat( book.getIsbn() , is("1234"));
+	}
+
+	@Test(expected=java.lang.IllegalArgumentException.class)
+	public void nullはISBNに設定できない() {
+		new Book().setIsbn(null);
+	}
+}
